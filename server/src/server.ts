bcae2e88fa582d680cfe02a962f041fd6298d172
RafaelVtor <rafaelvitor_as@hotmail.com>
2020-06-03 15:37:57 -0300

    Rotas de inserção em items e points criadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/user', (request, response)=>{
-  response.json([
-    'Rafael,',
-    'Cecília',
-    'Marina'    
-  ])
-  
-});
+app.use(express.json())
 
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.listen(3333);
