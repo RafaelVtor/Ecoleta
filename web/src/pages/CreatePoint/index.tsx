7c9a126e407d615e8b5676e388cdf49ec90fc4ac
RafaelVtor <rafaelvitor_as@hotmail.com>
2020-06-10 21:51:20 -0300

    adiconado informações do backend

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,20 +1,30 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
-import { Map,TileLayer, Marker} from 'react-leaflet'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 import api from '../../services/api'
 
 import './index.css'
 
-import logo from '../../assets/logo.svg' 
+import logo from '../../assets/logo.svg'
+
+interface Item {
+  id: number;
+  title: string;
+  image_url:string;
+}
 
 const CreatPoint = () => {
+  const [items, setItems] = useState<Item[]>([])
 
-  useEffect(()=>{
-    api.get('items').then(response =>{
-      console.log(response)
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data.serializedItems)
+      
     })
   }, []);
+
+  console.log(items)
 
   return (
     <div id='page-create-point'>
@@ -52,13 +62,12 @@ const CreatPoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-12.9131638,-38.3522319]} zoom = {15}>
-          <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[-12.9131638,-38.3522319]}/>
-
+          <Map center={[-12.9131638, -38.3522319]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+            <Marker position={[-12.9131638, -38.3522319]} />
           </Map>
 
           <div className='field-group'>
@@ -83,31 +92,18 @@ const CreatPoint = () => {
             <span>Selecione um ou mais ítens abaixo</span>
           </legend>
 
-          <ul className="items-grid">
-            <li className='selected'>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http:/localhost:3333/upload/oleo.svg" alt="teste"/>
-              <span>Óleo de Cozinha</span>
-            </li>
+          <ul className='items-grid'>
+            {items.map(item=>(
+
+              <li key={item.id}>
+                <img
+                  src={item.image_url}
+                  alt='teste'
+                  />
+                <span>{item.title}</span>
+              </li>
+                  ))}
+          
           </ul>
         </fieldset>
 
